0a7b720ff430096386e04b4418596f1ddfd4b1fd
doublenns <nnamdi.anyanwu@gmail.com>
2021-08-29 15:02:25 -0500

    Remove String Spaces in Go using unicode.IsSpace()

--- 8-kyu/remove_string_spaces/remove_string_spaces.go
+++ 8-kyu/remove_string_spaces/remove_string_spaces.go
@@ -1,11 +1,24 @@
 package remove_string_spaces
 
-import "strings"
+import (
+	"strings"
+	"unicode"
+)
 
 func NoSpace1(word string) string {
 	var result strings.Builder
 	for _, v := range word {
 		if v != ' ' {
+			result.WriteRune(v)
+		}
+	}
+	return result.String()
+}
+
+func NoSpace2(word string) string {
+	var result strings.Builder
+	for _, v := range word {
+		if !unicode.IsSpace(v) {
 			result.WriteRune(v)
 		}
 	}
