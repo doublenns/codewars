cf1215c5fb981a9f6358eb6d5968fb04d0fb8427
doublenns <nnamdi.anyanwu@gmail.com>
2021-08-29 14:41:43 -0500

    3rd solution of reversed_words.py using reversed() for reversal

--- 8-kyu/reversed_words/reversed_words.py
+++ 8-kyu/reversed_words/reversed_words.py
@@ -5,3 +5,12 @@ def reverse_words1(s):
         words[i], words[j] = words[j], words[i]
         i+=1; j-=1
     return " ".join(words)
+
+def reverse_words2(s):
+    words = s.split()
+    reversedWords = words[::-1]
+    return " ".join(reversedWords)
+
+def reverse_words3(s):
+    words = s.split()
+    return " ".join(reversed(words))
